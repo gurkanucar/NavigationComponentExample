1b82680076bd93e3d1b117c1fee1199d56eb90c1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-09 19:08:05 +0300

    first fragment to second fragment action implemented

--- app/src/main/java/com/gucarsoft/navigationcomponentexample/FirstFragment.kt
+++ app/src/main/java/com/gucarsoft/navigationcomponentexample/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,14 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val textView: TextView = view.findViewById(R.id.text_view_1)
+        textView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        return view
     }
 
     companion object {
